14a1efb99890ef557d404b267e711386a7705bff
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-13 19:17:02 -0400

    Create mac.py

---
+++ mac.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import subprocess
+# OS commands
+import optparse
+# user input commands
+
+
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+(options, arguments) = parser.parse_args()
+# capture arguments and options from user input, "-i" - option, "interface" - argument
+# options then arguments, contains user input options.new_mac
+
+# ctrl+d duplicate string
+# ctrl+/ comment string
+
+# variables:
+# interface = "eth0"
+# new_mac = "08:00:27:5b:b1:a6"
+
+# variables for input - options
+
+interface = options.interface
+new_mac = options.new_mac
+
+# user input:
+# interface = input("interface > ")
+# new_mac = input("new MAC > ")
+# for python2.7 raw_input("")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+# not checking user input, just string, payload: ifconfig; ls; down (;ls;)
+# to execute side commands!
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
+# handling user input commands,anti-hijacking
+
+# default: 
+# experimental: de:ad:be:ef:ca:fe
