8456d71b006f99fa5641e595dcfad0f32e43f577
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-14 05:37:20 -0400

    Create mac2.py

---
+++ mac2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#check output
+import subprocess
+import optparse
+import re
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change it's MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
+    # get this arguments outside
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print(
+            "[-] Could not read Mac address.")  # to execute programm without errors, lo interface doesn't have MAC adress
+
+
+(options, arguments) = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
+change_mac(options.interface, options.new_mac)
+# function inputs
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address did not get changed.")
